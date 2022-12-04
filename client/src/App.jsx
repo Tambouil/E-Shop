6b4ffc1d3612f82de31d2aea10a1d49b91757ce2
Valentin Berceaux <v.berceaux@gmail.com>
2022-12-04 12:05:32 +0100

    feat: layout for react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,41 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
-import Product from './pages/Product';
 import Products from './pages/Products';
+import Product from './pages/Product';
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/products/:id',
-    element: <Products />,
-  },
-  {
-    path: '/product/:id',
-    element: <Product />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/products/:id',
+        element: <Products />,
+      },
+      {
+        path: '/product/:id',
+        element: <Product />,
+      },
+    ],
   },
 ]);
+
 function App() {
   return <RouterProvider router={router} />;
 }
