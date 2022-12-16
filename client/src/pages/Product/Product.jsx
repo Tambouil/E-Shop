45808c412a0454b1ab8a4ae5079a1927a521d26f
Valentin Berceaux <v.berceaux@gmail.com>
2022-12-16 13:14:53 +0100

    refactor(product): fetch data fom strapi

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -2,68 +2,79 @@ import { useState } from 'react';
 import { BsCartPlusFill } from 'react-icons/bs';
 import { MdFavoriteBorder } from 'react-icons/md';
 import { FaBalanceScale } from 'react-icons/fa';
-
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
 import './Product.scss';
 
 const Product = () => {
+  const id = useParams().id;
   const [selectedImg, setSelectedImg] = useState('img');
   const [quantity, setQuantity] = useState(1);
 
-  const data = {
-    id: 1,
-    img: 'https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600',
-    img2: 'https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600',
-    title: 'Product 1',
-    isNew: true,
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
-    price: 50,
-  };
+  const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
 
   return (
     <div className="product">
-      <div className="left">
-        <div className="images">
-          <img src={data.img} alt="" onClick={(e) => setSelectedImg('img')} />
-          <img src={data.img2} alt="" onClick={(e) => setSelectedImg('img2')} />
-        </div>
-        <div className="mainImg">
-          <img src={data[selectedImg]} alt="" />
-        </div>
-      </div>
-      <div className="right">
-        <h1>{data.title}</h1>
-        <span className="price">${data.price}</span>
-        <p>{data.desc}</p>
-        <div className="quantity">
-          <button onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}>-</button>
-          {quantity}
-          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
-        </div>
-        <button className="add">
-          <BsCartPlusFill /> ADD TO CART
-        </button>
-        <div className="links">
-          <div className="item">
-            <MdFavoriteBorder /> ADD TO WISHLIST
+      {loading ? (
+        'loading'
+      ) : (
+        <>
+          <div className="left">
+            <div className="images">
+              <img
+                src={import.meta.env.VITE_STRAPI_BASE_URL + data?.attributes?.img?.data?.attributes?.url}
+                alt=""
+                onClick={(e) => setSelectedImg('img')}
+              />
+              <img
+                src={import.meta.env.VITE_STRAPI_BASE_URL + data?.attributes?.img2?.data?.attributes?.url}
+                alt=""
+                onClick={(e) => setSelectedImg('img2')}
+              />
+            </div>
+            <div className="mainImg">
+              <img
+                src={import.meta.env.VITE_STRAPI_BASE_URL + data?.attributes?.[selectedImg]?.data?.attributes?.url}
+                alt=""
+              />
+            </div>
           </div>
-          <div className="item">
-            <FaBalanceScale /> COMPARE
+          <div className="right">
+            <h1>{data?.attributes?.title}</h1>
+            <span className="price">${data?.attributes?.price}</span>
+            <p>{data?.attributes?.desc}</p>
+            <div className="quantity">
+              <button onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}>-</button>
+              {quantity}
+              <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+            </div>
+            <button className="add">
+              <BsCartPlusFill /> ADD TO CART
+            </button>
+            <div className="links">
+              <div className="item">
+                <MdFavoriteBorder /> ADD TO WISHLIST
+              </div>
+              <div className="item">
+                <FaBalanceScale /> COMPARE
+              </div>
+            </div>
+            <div className="info">
+              <span>Vendor: Polo</span>
+              <span>Product Type: T-Shirt</span>
+              <span>Tag: T-Shirt, Women, Top</span>
+            </div>
+            <hr />
+            <div className="info">
+              <span>DESCRIPTION</span>
+              <hr />
+              <span>ADDITIONAL INFORMATION</span>
+              <hr />
+              <span>FAQ</span>
+            </div>
           </div>
-        </div>
-        <div className="info">
-          <span>Vendor: Polo</span>
-          <span>Product Type: T-Shirt</span>
-          <span>Tag: T-Shirt, Women, Top</span>
-        </div>
-        <hr />
-        <div className="info">
-          <span>DESCRIPTION</span>
-          <hr />
-          <span>ADDITIONAL INFORMATION</span>
-          <hr />
-          <span>FAQ</span>
-        </div>
-      </div>
+        </>
+      )}
     </div>
   );
 };
